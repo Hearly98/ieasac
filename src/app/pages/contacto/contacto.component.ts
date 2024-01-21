df5cffa088124530c5bad0a2c3bdc9a196c12169
Sebastian <shuertas981@gmail.com>
2024-01-21 03:21:37 -0500

    Agregando estilos a la pagina contactanos y Nosotros

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { DatosComponent } from './components/datos/datos.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DatosComponent, FormularioComponent],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
